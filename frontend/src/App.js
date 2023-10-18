bba92b223436b52f42b03aa6e87f06d75f5026af
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-10-18 09:49:47 +0600

    router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,31 @@
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './containers/Home';
+import About from './containers/About';
+import Contact from './containers/Contact';
+import Listings from './containers/Listings';
+import ListingsDetails from './containers/ListingsDetails';
+import SignIn from './containers/SignIn';
+import SignUp from './containers/SignUp';
+
+import Layout  from './hocs/Layout';
+
+const App = () => {
   return (
-    <div>
-      app
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/listings" element={<Listings />} />
+          <Route path="/listings/:id" element={<ListingsDetails />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
